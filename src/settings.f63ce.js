86cc77940faa86190da7b04fa832a5263a6635a9
iwin88789 <134831098+iwin88789@users.noreply.github.com>
2023-07-18 16:40:08 +0700

    nd168

---
+++ src/settings.f63ce.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/Game.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"3c089",main:"a2bd1"}};
